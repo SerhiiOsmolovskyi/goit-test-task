c4571e099a71c86a461eae76b4dab1b2a65d9262
Serhii Osmolovskyi <osmolovsky.s@gmail.com>
2024-04-26 10:46:56 -0600

    basename corrected

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,13 @@ import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <BrowserRouter basename="/goit-test-task">
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="/goit-test-task">
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
