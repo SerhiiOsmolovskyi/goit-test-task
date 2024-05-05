11ee3260931534b0fcd87771b7788b63e670729a
SerhiiOsmolovskyi <109869085+SerhiiOsmolovskyi@users.noreply.github.com>
2024-05-05 17:59:59 +0000

    Deploying to gh-pages from @ SerhiiOsmolovskyi/goit-test-task@6f4bff631f83066a074d0a64ee26897f693c25b2 🚀

---
+++ assets/FavoritesPage-DkKr9diz.js
@@ -0,0 +1,2 @@
+import{u as t,j as s}from"./index-DBmzhAA3.js";import{c as o,C as r}from"./CamperList-Cq6DD6v8.js";const a="_section_1iry5_1",c={section:a};function m(){const e=t(o);return s.jsx("section",{className:c.section,children:s.jsx(r,{campers:e})})}export{m as default};
+//# sourceMappingURL=FavoritesPage-DkKr9diz.js.map
--- assets/FavoritesPage-LwMIXaOb.js
+++
@@ -1,2 +0,0 @@
-import{u as t,j as s}from"./index-BrlcfR77.js";import{c as o,C as r}from"./CamperList-CVTd14Rj.js";const a="_section_1iry5_1",c={section:a};function m(){const e=t(o);return s.jsx("section",{className:c.section,children:s.jsx(r,{campers:e})})}export{m as default};
-//# sourceMappingURL=FavoritesPage-LwMIXaOb.js.map
